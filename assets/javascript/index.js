8716a25781fdf104d37434c7a3e2b4a7f7194bcf
CaioAbra <caioabra41@gmail.com>
2024-09-14 02:26:07 -0300

    quase

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,41 +1,121 @@
 $(document).ready(function () {
-    // $('#sidebarCollapse').on('click', function () {
-    //     $('#sidebar').toggleClass('active');
 
-    //     // if ($('#sidebar').hasClass('active')) {
-    //     $('.iconDesactive').toggleClass('d-none');
-    //     $('.iconActive').toggleClass('d-none');
+    // var ctx = document.getElementById('visaoGeralChart').getContext('2d');
+    var ctx = document.getElementById('visaoGeralChart').getContext('2d');
 
-    //     $('.sidebar-header div.img-logo').toggleClass('col-8');
-    //     $('.sidebar-header div.img-logo').toggleClass('mb-3');
-    //     $('.sidebar-header div.btn-sidebar').toggleClass('col-4');
-    //     // }
-    // });
-
-
-    var ctx = document.getElementById('chart').getContext('2d');
     var chart = new Chart(ctx, {
-        type: 'bar',
+        type: 'doughnut',
         data: {
-            labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set'],
+            labels: [
+                'Depósitos',
+                'Depositantes',
+                'FTD’s'
+            ],
             datasets: [{
-                label: 'Receita',
-                data: [10000, 12000, 14000, 13000, 15000, 16000, 18000, 20000, 22000],
-                backgroundColor: '#DE8734',
+                label: 'My First Dataset',
+                data: [300, 200, 47],
+                backgroundColor: [
+                    '#DE8734',
+                    '#B9B9B9',
+                    '#4D4D4D'
+                ],
+                hoverOffset: 4
+            }]
+        }
+    });
+
+    var ctxdois = document.getElementById('visaoGeralChartdois').getContext('2d');
+
+    var chart = new Chart(ctxdois, {
+        type: 'doughnut',
+        data: {
+            labels: [
+                'Depósitos',
+                'Depositantes',
+                'FTD’s'
+            ],
+            datasets: [{
+                label: 'My First Dataset',
+                data: [300, 200, 47],
+                backgroundColor: [
+                    '#DE8734',
+                    '#B9B9B9',
+                    '#4D4D4D'
+                ],
+                hoverOffset: 4
+            }]
+        }
+    });
+
+    var graficoCrescimentoChart = document.getElementById('graficoCrescimentoChart').getContext('2d');
+
+    var labels = ['JAN', 'FEV', 'MAR', 'ABR', 'MAI', 'JUN', 'JUL', 'AGO', 'SET']; // Definir os rótulos
+
+    var chart = new Chart(graficoCrescimentoChart, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: '', // Deixe o label vazio se quiser esconder o texto da legenda
+                data: [30000, 60000, 40000, 100000, 40000, 80000, 40000, 60000, 70000], // Valores numéricos
+                fill: false,
                 borderColor: '#DE8734',
-                borderWidth: 1
+                tension: 0.2
             }]
         },
         options: {
+            legend: {
+                display: false // Esconder a legenda
+            },
             scales: {
-                y: {
-                    beginAtZero: true
-                }
+                yAxes: [{
+                    ticks: {
+                        // Formatar os valores do eixo Y com "K"
+                        callback: function (value, index, values) {
+                            return value / 1000 + 'K'; // Exibe como "K"
+                        }
+                    }
+                }]
             }
         }
     });
 
-    // if(('#sidebar').hasClass){}
+
+
+    var graficoCrescimentoChartdois = document.getElementById('graficoCrescimentoChartdois').getContext('2d');
+
+    var labels = ['JAN', 'FEV', 'MAR', 'ABR', 'MAI', 'JUN', 'JUL', 'AGO', 'SET']; // Definir os rótulos
+
+    var chart = new Chart(graficoCrescimentoChartdois, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: '', // Deixe o label vazio se quiser esconder o texto da legenda
+                data: [30000, 60000, 40000, 100000, 40000, 80000, 40000, 60000, 70000], // Valores numéricos
+                fill: false,
+                borderColor: '#DE8734',
+                tension: 0.2
+            }]
+        },
+        options: {
+            legend: {
+                display: false // Esconder a legenda
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        // Formatar os valores do eixo Y com "K"
+                        callback: function (value, index, values) {
+                            return value / 1000 + 'K'; // Exibe como "K"
+                        }
+                    }
+                }]
+            }
+        }
+    });
+
+
 });
 
 $('button.togle').on('click', function () {
